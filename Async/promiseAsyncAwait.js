bfdf9fda4d564fcc2904303abe88c1f645db1571
7AkshayAnand <akshayjha560@gmail.com>
2024-01-05 16:09:18 +0530

    add new projects

--- Async/promiseAsyncAwait.js
+++ Async/promiseAsyncAwait.js
@@ -1,50 +1,47 @@
-function getCheese(){
+function getCheese() {
 
-    return new Promise(function(resolve,reject){
-        setTimeout(()=>{
-            const cheese="🍕"
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            const cheese = "🍕"
             resolve(cheese)
-        },2000)
+        }, 2000)
     })
-    
-}
-function makeDough(cheese){
 
-    return new Promise(function(resolve,reject){
-        setTimeout(()=>{
-            const dough=cheese+"🍩"
+}
+function makeDough(cheese) {
+
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            const dough = cheese + "🍩"
             // console.log('here is the dough ',dough);
             resolve(dough)
             reject("BAD cheese")
-        },2000)
+        }, 2000)
     })
-    
-}
-function makePizza(dough){
 
-    return new Promise(function(resolve,reject){
-        setTimeout(()=>{
-            const pizza=dough+"🧀"
+}
+function makePizza(dough) {
+
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            const pizza = dough + "🧀"
             // console.log('here is the pizza ',pizza);
             resolve(pizza)
-        },2000)
+        }, 2000)
     })
-    
+
 }
 
-async function orderPizza(){
-   
-
-
-    try{
-         const cheese=await getCheese()
-    console.log("here is the cheese",cheese);
-    const dough=await makeDough(cheese)
-    console.log("here is the dough",dough);
-    const pizza=await makePizza(dough)
-    console.log("here is the pizza",pizza);
-    }catch(error){
-         console.error("error occoured");
+async function orderPizza() {
+    try {
+        const cheese = await getCheese()
+        console.log("here is the cheese", cheese);
+        const dough = await makeDough(cheese)
+        console.log("here is the dough", dough);
+        const pizza = await makePizza(dough)
+        console.log("here is the pizza", pizza);
+    } catch (error) {
+        console.error("error occoured");
     }
 
 }
